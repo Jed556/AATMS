856a7dcec0aafe8836daaea1f37d47c4f05eb17f
Jed556 <jguiriba11@gmail.com>
2023-06-08 22:08:38 +0800

    feat: Make font and GUI handlers

--- include/AATMS.h
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
---
+++ src/system/AATMS.h
@@ -0,0 +1,8 @@
+#ifndef AATMS_H
+#define AATMS_H
+
+#include "database.h"
+#include "loan.h"
+#include "interface.h"
+
+#endif  // AATMS_H
